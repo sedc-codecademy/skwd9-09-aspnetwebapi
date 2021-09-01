4ff2bfbb7dba38f3150a771a1b1b227478e523ef
Tanja Stojanovska <tanja.stojanovska@twelve.eu>
2021-09-01 20:50:37 +0200

    Class 01

---
+++ G2/Class01/Code/ExampleWebApiApp/ExampleWebApiApp/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("strings")]
+        public ActionResult<IEnumerable<string>> GetStrings()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("integer")]
+        public ActionResult<int> GetInteger()
+        {
+            return 5;
+        }
+    }
+}
