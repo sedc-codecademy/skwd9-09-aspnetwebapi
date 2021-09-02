216b4c2acab9dff48a3b7185d6e6179c2b6ebc20
LudNaucnik <bajraktarovljube@gmail.com>
2021-09-02 20:40:06 +0200

    G6 Class 01

---
+++ G6/Class 01/WebApplication3/WebApplication3/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "Ljube" });
+            users.Add(new User() { Id = 2, Name = "Petre" });
+
+            return BadRequest(users);
+        }
+
+        [HttpPost]
+        public List<User> AddNewUser(User user)
+        {
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "Petre" });
+            users.Add(user);
+
+            return users;
+        }
+    }
+}
---
+++ G6/Class 01/WebApplication3/WebApplication3/User.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication3
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
