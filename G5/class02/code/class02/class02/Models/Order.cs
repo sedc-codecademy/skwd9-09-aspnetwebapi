7df7ce878eba5ec8790fc6dcc29adc9defe3062a
Viktor Jakovlev <vicebad@hotmail.com>
2021-09-07 21:01:25 +0200

    class03 finished

---
+++ G5/class02/code/class02/class02/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace class02.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int OrderNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
