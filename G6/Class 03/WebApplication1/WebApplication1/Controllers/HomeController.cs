89754eb2311a0a7373697b21206a963bda18f334
LudNaucnik <bajraktarovljube@gmail.com>
2021-09-07 21:02:11 +0200

    G6 Class 03

---
+++ G6/Class 03/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
