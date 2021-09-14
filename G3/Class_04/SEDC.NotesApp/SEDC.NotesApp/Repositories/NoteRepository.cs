e454db1aa8547b0ea6cbdff36ca52a2f62038e4b
Goce Kabov <Goce.Kabov@seavus.com>
2021-09-14 18:47:59 +0200

    added class 04

---
+++ G3/Class_04/SEDC.NotesApp/SEDC.NotesApp/Repositories/NoteRepository.cs
@@ -0,0 +1,54 @@
+﻿using SEDC.NotesApp.Models.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.NotesApp.Repositories
+{
+    public class NoteRepository : IRepository<Note>
+    {
+        public void Add(Note entity)
+        {
+            entity.Id = StaticDb.Notes.Count + 1;
+            StaticDb.Notes.Add(entity);
+        }
+
+        public List<Note> GetAll()
+        {
+            return StaticDb.Notes;
+        }
+
+        public Note GetById(int id)
+        {
+            return StaticDb.Notes.SingleOrDefault(note => note.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            Note note = StaticDb.Notes.SingleOrDefault(note => note.Id == id);
+
+            //if (note != null)
+            //{
+            //    StaticDb.Notes.Remove(note);
+            //}
+
+            if (note == null)
+            {
+                throw new Exception("No such note!");
+            }
+            StaticDb.Notes.Remove(note);
+        }
+
+        public void Update(Note entity)
+        {
+            Note note = StaticDb.Notes.SingleOrDefault(note => note.Id == entity.Id);
+            if (note == null)
+            {
+                throw new Exception("No such note");
+            }
+            int idxOfExistingNote = StaticDb.Notes.IndexOf(note);
+            StaticDb.Notes[idxOfExistingNote] = entity;
+        }
+    }
+}
