ff4fbc524234b26d9c730eadc58d5d75b71e8dd7
Martin <panovski.martin93@gmail.com>
2021-09-20 21:04:00 +0200

    Class 06 demo code added

---
+++ G1/Class 06/Class06.EntityFramework/Class06.EntityFramework.DataModels/CreatedFromDb/Notes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class06.EntityFramework.DataModels.CreatedFromDb
+{
+    public partial class Notes
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int UserId { get; set; }
+
+        public virtual Users User { get; set; }
+
+        public string Color { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public int PriorityLevel { get; set; }
+    }
+}
---
+++ G1/Class 06/Class06.EntityFramework/Class06.EntityFramework.DataModels/Migrations/20210920182414_NotesDomainModelExtended.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Class06.EntityFramework.DataModels.Migrations
+{
+    public partial class NotesDomainModelExtended : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PriorityLevel",
+                table: "Notes",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriorityLevel",
+                table: "Notes");
+        }
+    }
+}
