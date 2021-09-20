08d61774457d0c73fd8c961a3fbc991f66799669
Manass2 <panemanass2@gmail.com>
2021-09-20 21:23:40 +0200

    class 06

---
+++ G4/Class06/dataAnotations/SEDC.NotesApp.Helpers/DependencyInjectionHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SEDC.NotesApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.NotesApp.Helpers
+{
+    public static class DependencyInjectionHelper
+    {
+        public static void InjectDbContext(IServiceCollection services)
+        {
+            services.AddDbContext<NotesAppDbContext>(x =>
+                x.UseSqlServer("Server=DESKTOP-VSQ43CM\\SQLEXPRESS;Database=NotesDADb;Trusted_Connection=True"));
+        }
+    }
+}
