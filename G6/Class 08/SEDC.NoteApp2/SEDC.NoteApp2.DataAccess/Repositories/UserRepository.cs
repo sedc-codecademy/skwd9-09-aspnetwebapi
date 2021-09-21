a17a799f4e3f961fe39bb7824f8a3a7bdb7a7a84
LudNaucnik <bajraktarovljube@gmail.com>
2021-09-21 18:51:20 +0200

    Class 08 CodeV1

---
+++ G6/Class 08/SEDC.NoteApp2/SEDC.NoteApp2.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using SEDC.NoteApp2.DataAccess.Interfaces;
+using SEDC.NoteApp2.Domain.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SEDC.NoteApp2.DataAccess.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public void Add(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAllIncludeNotes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetByIdIncludeNotes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ G6/Class 08/SEDC.NoteApp2/SEDC.NoteApp2.Domain/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using SEDC.NoteApp2.Shared.Enums;
+
+namespace SEDC.NoteApp2.Domain.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Color { get; set; }
+        public TagType Tag { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
