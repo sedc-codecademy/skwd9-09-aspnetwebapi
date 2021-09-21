8599438fbc2f7b4b71b036d482e3c36bdf1dc267
LudNaucnik <bajraktarovljube@gmail.com>
2021-09-21 19:08:17 +0200

    added dto models

---
+++ G6/Class 08/SEDC.NoteApp2/SEDC.NoteApp2.Dto/Models/NoteDto.cs
@@ -0,0 +1,14 @@
+﻿using SEDC.NoteApp2.Shared.Enums;
+
+namespace SEDC.NoteApp2.Dto.Models
+{
+    public class NoteDto
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Color { get; set; }
+        public TagType Tag { get; set; }
+        public int UserId { get; set; }
+        public string UserFullName { get; set; }
+    }
+}
