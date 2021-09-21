d319f44720bef7dd66c29abfa8ca2fa33d1c7851
LudNaucnik <bajraktarovljube@gmail.com>
2021-09-21 19:03:12 +0200

    added initial migration

--- G6/Class 08/SEDC.NoteApp2/SEDC.NoteApp2.API/Startup.cs
+++ G6/Class 08/SEDC.NoteApp2/SEDC.NoteApp2.API/Startup.cs
@@ -2,11 +2,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using SEDC.NoteApp2.DataAccess.Interfaces;
+using SEDC.NoteApp2.DataAccess.Repositories;
+using SEDC.NoteApp2.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +36,13 @@ namespace SEDC.NoteApp2.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SEDC.NoteApp2.API", Version = "v1" });
             });
+
+            services.AddDbContext<NotesDbContext>(
+                options => options.UseSqlServer("Server=.\\SQLEXPRESS;Database=Notes;Trusted_Connection=True;")
+                );
+
+            services.AddTransient<INoteRepository, NoteRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
