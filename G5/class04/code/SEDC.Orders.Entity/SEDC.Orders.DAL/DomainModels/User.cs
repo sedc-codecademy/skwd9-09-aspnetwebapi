bd302e8bc3566fd9a4249797539f119d0768a60e
Viktor Jakovlev <vicebad@hotmail.com>
2021-09-23 19:02:27 +0200

    updates

--- G5/class04/code/SEDC.Orders.Entity/SEDC.Orders.DAL/DomainModels/User.cs
+++ G5/class04/code/SEDC.Orders.Entity/SEDC.Orders.DAL/DomainModels/User.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace SEDC.Orders.DAL.DomainModels
 {
+    //[Table("Korisink")]
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
+        //[MaxLength(30)]
+        //[Required]
+        //[Column("order-number")]
         public string FullName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+
+
+        //navigation properties 
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }
---
+++ G5/class04/code/SEDC.Orders.Entity/SEDC.Orders.DAL/Migrations/20210923170149_init.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SEDC.Orders.DAL.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ordernumber = table.Column<int>(name: "order-number", nullable: false),
+                    Address = table.Column<string>(maxLength: 30, nullable: true),
+                    IsDelievered = table.Column<bool>(nullable: false),
+                    OrderCreationDate = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "MyFKConstraint",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
