31a359f0d88a53670733051268cb1918d027c5d3
tstojanovska <stojanovskatanja03@gmail.com>
2021-09-25 12:28:49 +0200

    Class 08

---
+++ G2/Class08/Code/SEDC.NotesApp/SEDC.NotesApp/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SEDC.NotesApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.NotesApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            //get the value for AppSettings property in appSettings.json
+            var appSettingsConfiguration = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsConfiguration);
+
+            AppSettings appSettings = appSettingsConfiguration.Get<AppSettings>();
+
+            DependencyInjectionHelper.InjectDbContext(services, appSettings.DbConnectionString);
+            DependencyInjectionHelper.InjectRepositories(services);
+            //DependencyInjectionHelper.InjectAdoRepositories(services, appSettings.DbConnectionString);
+            DependencyInjectionHelper.InjectDapperRepositories(services, appSettings.DbConnectionString);
+            DependencyInjectionHelper.InjectServices(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
