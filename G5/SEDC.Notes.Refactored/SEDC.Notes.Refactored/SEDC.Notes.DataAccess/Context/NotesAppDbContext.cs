624d02815ab78c3454c24217ad7fff04e7d10b03
Viktor Jakovlev <vicebad@hotmail.com>
2021-09-28 19:55:29 +0200

    Notes.Refactored initial commit

---
+++ G5/SEDC.Notes.Refactored/SEDC.Notes.Refactored/SEDC.Notes.DataAccess/Context/NotesAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEDC.Notes.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Notes.DataAccess.Context
+{
+    //Microsoft.EntityFrameworkCore.SqlServer
+    //Microsoft.EntityFrameworkCore.SqlServer.Design
+    //Microsoft.EntityFrameworkCore.Tools
+    public class NotesAppDbContext : DbContext
+    {
+        public NotesAppDbContext(DbContextOptions<NotesAppDbContext> options) : base(options) { }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Note> Notes { get; set; }
+    }
+}
