3b6376b0ead59b36f2f6c500b02379b2a410deee
kristina-s <spasevska.kristina87@gmail.com>
2021-09-29 21:34:26 +0200

    G4 class 08 added

---
+++ G4/Class08/Homework-class08.md
@@ -0,0 +1,14 @@
+# Homework - Class 08
+
+Use the SEDC.NotesApp project from Class08
+
+1. Add UserRepository 
+
+2. Implement the GetAll, GetById, Add, Update and Delete methods for users using ADO.NET and Dapper
+
+When implementing the CRUD operations use only the primitive properties (Id, Firstname, LastName, Username).
+
+## Contact
+Trainer: panemanaskov@gmail.com
+
+Trainer: spasevska.kristina87@gmail.com
---
+++ G4/Class08/SEDC.NotesAPI/SEDC.NotesAPI.Domain/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.NotesAPI.Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; } // recognizes Id as key (PK)
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Username { get; set; }
+        public int Age { get; set; }
+
+        public List<Note> Notes { get; set; }
+    }
+}
