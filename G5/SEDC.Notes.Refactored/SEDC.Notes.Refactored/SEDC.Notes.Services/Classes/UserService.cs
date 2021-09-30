6ce74aaedfb43efd02185efecf289abb5fad70e4
Viktor Jakovlev <vicebad@hotmail.com>
2021-09-30 19:08:57 +0200

    updates

---
+++ G5/SEDC.Notes.Refactored/SEDC.Notes.Refactored/SEDC.Notes.Services/Classes/UserService.cs
@@ -0,0 +1,44 @@
+﻿using SEDC.Notes.DataAccess.Repositories.Interfaces;
+using SEDC.Notes.DomainModels;
+using SEDC.Notes.RequestModels;
+using SEDC.Notes.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SEDC.Notes.Services.Classes
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> _userRepositoty;
+
+        public UserService(IRepository<User> userRepositoty)
+        {
+            _userRepositoty = userRepositoty;
+        }
+
+
+        public void Register(RegisterRequestModel requestModel)
+        {
+            if (requestModel.Password != requestModel.ConfirmPassword) 
+            {
+                //throw new Exception("Password did not match!");
+            }
+
+            var md5 = new MD5CryptoServiceProvider();
+            var md5Data = md5.ComputeHash(Encoding.ASCII.GetBytes(requestModel.Password));
+            var hashedPassword = Encoding.ASCII.GetString(md5Data);
+
+            var user = new User()
+            {
+                Username = requestModel.Username,
+                FirstName = requestModel.FirstName,
+                LastName = requestModel.LastName,
+                Password = hashedPassword
+            };
+
+            _userRepositoty.Add(user);
+        }
+    }
+}
---
+++ G5/SEDC.Notes.Refactored/SEDC.Notes.Refactored/SEDC.Notes.Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using SEDC.Notes.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Notes.Services.Interfaces
+{
+    public interface IUserService
+    {
+        void Register(RegisterRequestModel requestModel);
+    }
+}
