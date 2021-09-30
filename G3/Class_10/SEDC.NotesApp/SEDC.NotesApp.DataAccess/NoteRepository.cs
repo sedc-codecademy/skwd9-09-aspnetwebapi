94e85866a2f9e0ab8162d29d45f7e15ee91c2b8b
Goce.Kabov <45940739+GK0208@users.noreply.github.com>
2021-09-30 21:04:35 +0200

    added class 10

---
+++ G3/Class_10/SEDC.NotesApp/SEDC.NotesApp.DataAccess/NoteRepository.cs
@@ -0,0 +1,52 @@
+﻿using SEDC.NotesApp.Models;
+using SEDC.NotesApp.Models.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.NotesApp.Repositories
+{
+    public class NoteRepository : IRepository<Note>
+    {
+        private readonly NotesAppDbContext _dbContext;
+
+        public NoteRepository(NotesAppDbContext context)
+        {
+            _dbContext = context;
+        }
+        public void Add(Note entity)
+        {
+            _dbContext.Notes.Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public List<Note> GetAll()
+        {
+            return _dbContext.Notes.ToList();
+        }
+
+        public Note GetById(int id)
+        {
+            return _dbContext.Notes.SingleOrDefault(note => note.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            Note note = _dbContext.Notes.SingleOrDefault(note => note.Id == id);
+            _dbContext.Notes.Remove(note);
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(Note entity)
+        {
+            Note note = _dbContext.Notes.SingleOrDefault(note => note.Id == entity.Id);
+            note.Color = entity.Color;
+            note.Tag = entity.Tag;
+            note.UserId = entity.UserId;
+            note.Text = entity.Text;
+            _dbContext.SaveChanges();
+            
+        }
+    }
+}
