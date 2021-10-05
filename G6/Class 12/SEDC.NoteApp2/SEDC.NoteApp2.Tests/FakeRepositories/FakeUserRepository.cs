3340a97f96ffe421c6b6f24170435b108223dfcb
LudNaucnik <bajraktarovljube@gmail.com>
2021-10-05 19:17:33 +0200

    added unit tests

---
+++ G6/Class 12/SEDC.NoteApp2/SEDC.NoteApp2.Tests/FakeRepositories/FakeUserRepository.cs
@@ -0,0 +1,77 @@
+﻿using SEDC.NoteApp2.DataAccess.Interfaces;
+using SEDC.NoteApp2.Domain.Models;
+using SEDC.NoteApp2.Shared.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SEDC.NoteApp2.Tests.FakeRepositories
+{
+    public class FakeUserRepository : IUserRepository
+    {
+        private List<User> _users;
+
+        public FakeUserRepository()
+        {
+            _users = new List<User>()
+            {
+                new User()
+                {
+                    Id = 1,
+                    FirstName = "Bob",
+                    LastName = "Bobsky",
+                    Username = "bob007",
+                    Password = "123456sedc".GenerateMD5(),
+                    Address = "Macedonia",
+                    Age = 30,
+                    Notes = new List<Note>()
+                }
+            };
+        }
+
+        public void Add(User entity)
+        {
+            _users.Add(entity);
+        }
+
+        public void Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAllIncludeNotes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            return _users.FirstOrDefault(q => q.Id == id);
+        }
+
+        public User GetByIdIncludeNotes(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserByUsernameAndPassword(string username, string password)
+        {
+            return _users.SingleOrDefault(q => q.Username == username && q.Password == password);
+        }
+
+        public bool IsUsernameInUse(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ G6/Class 12/SEDC.NoteApp2/SEDC.NoteApp2.Tests/UserTestsFakeRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SEDC.NoteApp2.Dto.Models;
+using SEDC.NoteApp2.Services.Implementations;
+using SEDC.NoteApp2.Services.Interfaces;
+using SEDC.NoteApp2.Shared;
+using SEDC.NoteApp2.Tests.FakeRepositories;
+
+namespace SEDC.NoteApp2.Tests
+{
+    [TestClass]
+    public class UserTestsFakeRepository
+    {
+        [TestMethod]
+        public void Authenticate_ValidUsernamePassword_ValidToken()
+        {
+            // Arrange
+            IOptions<AppSettings> options =
+                Options.Create(
+                    new AppSettings
+                    {
+                        Secret = "DkYzU7ypt2UhywG3"
+                    });
+
+            IUserService userService = new UserService(new FakeUserRepository(), options);
+            string username = "bob007";
+            string password = "123456sedc";
+
+            // Act
+            TokenDto result = userService.Authenticate(username, password);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(!string.IsNullOrEmpty(result.Token));
+        }
+    }
+}
