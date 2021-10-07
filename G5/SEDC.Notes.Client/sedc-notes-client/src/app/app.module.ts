35ec15e833f6e52f77a8b80e540541ca0768a3c7
Viktor Jakovlev <vicebad@hotmail.com>
2021-10-07 21:15:18 +0200

    angular client created

---
+++ G5/SEDC.Notes.Client/sedc-notes-client/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav-component/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { AppRoutingModule } from './app.routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CollapseModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ G5/SEDC.Notes.Client/sedc-notes-client/src/app/app.routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+
+const routes: Routes = [
+    {path: 'home', pathMatch: 'full', component: HomeComponent},
+    {path: 'register', pathMatch: 'full', component: RegisterComponent},
+    {path: 'login', pathMatch: 'full', component: LoginComponent},
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
