bdeec6320a151f0dff1861dfabb3443df8e5c7e2
Goce.Kabov <45940739+GK0208@users.noreply.github.com>
2021-10-07 19:40:52 +0200

    added class 13

---
+++ G3/Class_13/SEDC.NotesApp/SEDC.NotesApp.DataAccess/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.NotesApp.Repositories
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Remove(int id);
+        void Update(T entity);
+    }
+}
