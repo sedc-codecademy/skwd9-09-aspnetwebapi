7927199192dde4e943729aaa47024d57ae22d4ae
LudNaucnik <bajraktarovljube@gmail.com>
2021-10-07 18:30:01 +0200

    added serilog to application

--- G6/Class 13/SEDC.NoteApp2/SEDC.NoteApp2.API/Program.cs
+++ G6/Class 13/SEDC.NoteApp2/SEDC.NoteApp2.API/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
 
 namespace SEDC.NoteApp2.API
 {
@@ -7,11 +10,22 @@ namespace SEDC.NoteApp2.API
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Information()
+                .WriteTo.File(
+                $@"{AppDomain.CurrentDomain.BaseDirectory}Logs\Note-Log-{DateTime.UtcNow.Date:dd-MM-yyyy}.log",
+                LogEventLevel.Information,
+                "{NewLine}{Timestamp:HH:mm:ss} [{Level}] {Message} {Exception}"
+                )
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
