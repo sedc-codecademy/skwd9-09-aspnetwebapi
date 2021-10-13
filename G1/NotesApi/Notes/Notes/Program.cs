f14c848dcb92e205a504fcfd865dbb9d12c5ea1f
Martin <panovski.martin93@gmail.com>
2021-10-13 21:05:06 +0200

    Class 13 demo code added

--- G1/NotesApi/Notes/Notes/Program.cs
+++ G1/NotesApi/Notes/Notes/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SEDC.NotesApp.Api
 {
@@ -13,11 +10,21 @@ namespace SEDC.NotesApp.Api
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Information()
+                .WriteTo.RollingFile(
+                $@"{ Environment.CurrentDirectory }Logs\Notes_LOG_{DateTime.UtcNow.Date:dd-MM-yyyy}.txt",
+                LogEventLevel.Information,
+                "{NewLine}{Timestamp:HH:mm:ss} [{Level}] ({CorrelationToken}) {Message}{NewLine}{Exception}")
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
