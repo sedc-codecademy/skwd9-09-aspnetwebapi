dd63932d25082ad6518e508a35af8c427c9a39b2
Goce.Kabov <45940739+GK0208@users.noreply.github.com>
2021-10-14 20:55:18 +0200

    added class 15

---
+++ G3/Class_15/SEDC.NotesApp/SEDC.NotesApp.Domain/DbModels/User.cs
@@ -0,0 +1,25 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.NotesApp.Models.DbModels
+{
+    public class User
+    {
+        public User()
+        {
+            Notes = new List<Note>();
+        }
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+
+        [Computed]
+        public List<Note> Notes { get; set; }
+    }
+}
---
+++ G3/Class_15/SEDC.NotesApp/SEDC.NotesApp.Services/MappingProfiles/MappingProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using SEDC.NotesApp.DtoModels;
+using SEDC.NotesApp.Models.DbModels;
+using SEDC.NotesApp.Models.DtoModels;
+using SEDC.NotesApp.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.NotesApp.Services.MappingProfiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Note, NoteDto>()
+                .ForMember(dest=> dest.NoteText,
+                opt => opt.MapFrom(src => src.Text))
+                .ForMember(dest => dest.NoteColor,
+                opt => opt.MapFrom(src => src.Color))
+                .ForMember(dest => dest.Tag,
+                opt => opt.MapFrom(src => GetTag((int)src.Tag))
+                )
+                .ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+        }
+
+        private string GetTag(int tag)
+        {
+            if (tag == 3)
+            {
+                return "Home";
+            }
+            return "Other";
+        }
+    }
+}
